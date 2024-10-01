2450c9f0c48d70d5a09f7e3562700d2fa1477726
Arthur Heymans <arthur@aheymans.xyz>
2024-10-01 12:00:46 +0200

    Add zswap
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- modules/system.nix
+++ modules/system.nix
@@ -84,6 +84,7 @@
 
   programs.dconf.enable = true;
 
+  # TODO move all networking into a separate file
   networking.firewall.enable = true;
 
   # Enable the OpenSSH daemon.
@@ -169,6 +170,22 @@
       fi
     '';
 
-  # Add standard dirs
-  # services.xdg-user-dirs.enable = true;
+  # Enable in-memory compressed devices and swap space provided by the zram kernel module.
+  # By enable this, we can store more data in memory instead of fallback to disk-based swap devices directly,
+  # and thus improve I/O performance when we have a lot of memory.
+  #
+  #   https://www.kernel.org/doc/Documentation/blockdev/zram.txt
+  zramSwap = {
+    enable = true;
+    # one of "lzo", "lz4", "zstd"
+    algorithm = "lz4";
+    # Priority of the zram swap devices.
+    # It should be a number higher than the priority of your disk-based swap devices
+    # (so that the system will fill the zram swap devices before falling back to disk swap).
+    priority = 5;
+    # Maximum total amount of memory that can be stored in the zram swap devices (as a percentage of your total memory).
+    # Defaults to 1/2 of your total RAM. Run zramctl to check how good memory is compressed.
+    # This doesn’t define how much memory will be used by the zram swap devices.
+    memoryPercent = 50;
+  };
 }
