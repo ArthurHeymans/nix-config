1f8b6c88e14184e708a3cb8233e9764593bf2c3f
Arthur Heymans <arthur@aheymans.xyz>
2024-10-15 18:17:48 +0200

    Configure waybar like main system
    - Clock
    - Pulseaudio
    - Backlight
    - Temp
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/sway/waybar.nix
+++ home/sway/waybar.nix
@@ -18,17 +18,61 @@
         ];
         modules-center = [
           "sway/window"
-          "custom/hello-from-waybar"
         ];
         modules-right = [
+          "pulseaudio"
           "mpd"
-          "custom/mymodule#with-css-id"
           "temperature"
           "backlight"
           "battery"
           "clock"
           "tray"
         ];
+        temperature = {
+          critical-threshold = 85;
+          format = "{temperatureC}°C {icon}";
+          format-icons = ["" "" ""];
+        };
+        backlight = {
+          format = "{percent}% {icon}";
+          format-icons = ["" ""];
+        };
+        battery = {
+          states = {
+            # "good": 95,
+            warning = 30;
+            critical = 15;
+          };
+          format = "{capacity}% {icon}";
+          format-charging = "{capacity}% ";
+          format-plugged = "{capacity}% ";
+          format-alt = "{time} {icon}";
+          # "format-good": "" # // An empty format will hide the module
+          # "format-full": "",
+          format-icons = ["" "" "" "" ""];
+        };
+        pulseaudio = {
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-muted = " {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable =  "";
+            car = "";
+            default = ["" "" ""];
+          };
+          on-click = "pavucontrol";
+        };
+        clock = {
+          interval = 1;
+          format = "{:%d-%m-%Y %H:%M:%S}";
+        };
       };
     };
   };
