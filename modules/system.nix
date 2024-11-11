b65553846bf20637f5de8c92f9378e2f366987ec
Arthur Heymans <arthur@aheymans.xyz>
2024-11-11 12:21:38 +0100

    Move binary cache to nix.settings
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- modules/system.nix
+++ modules/system.nix
@@ -37,6 +37,15 @@
     ];
 
     trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
+
+    extra-substituters = [
+      "https://anyrun.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+    ];
+
     builders-use-substitutes = true;
   };
 
