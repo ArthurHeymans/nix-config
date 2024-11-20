5aad25a51b64127e5684130c55fc3c7b16344be3
Arthur Heymans <arthur@aheymans.xyz>
2024-11-20 21:34:41 +0100

    Hyprland: Lid switches
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -180,6 +180,9 @@
         ", XF86AudioPause, exec, playerctl play-pause"
         ", XF86AudioPlay, exec, playerctl play-pause"
         ", XF86AudioPrev, exec, playerctl previous"
+
+        ",switch:on:Lid Switch, exec, hyprctl keyword monitor \"eDP-1, disable\""
+        ",switch:off:Lid Switch, exec, hyprctl keyword monitor \"eDP-1, enable\""
       ];
 
       windowrulev2 = [
