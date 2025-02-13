c079f70862e16d186101de25886e0fd503aa7124
Arthur Heymans <arthur@aheymans.xyz>
2025-02-13 14:55:52 +0100

    Disable touchpad on hyprland
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -213,6 +213,14 @@
         # Fix some dragging issues with XWayland
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
+
+      # device specific settings
+      device = [
+        {
+          name = "synps/2-synaptics-touchpad";
+          enabled = "false";
+        }
+      ];
     };
   };
 }
