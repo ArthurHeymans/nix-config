fb72c14be4d7a002e88da85b6d70c5888d05c775
Arthur Heymans <arthur@aheymans.xyz>
2025-02-17 21:21:12 +0100

    Fix oopsie on hypridle
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/hyprland/hypridle.nix
+++ home/hyprland/hypridle.nix
@@ -3,17 +3,17 @@
     enable = true;
     settings = {
       general = {
-        lock_cmd = "hyprlock"; # dbus/sysd lock command (loginctl lock-session)
+        lock_cmd = "pidof hyprlock || hyprlock"; # dbus/sysd lock command (loginctl lock-session)
         unlock_cmd = ""; # same as above, but unlock
-        before_sleep_cmd = "hyprlock"; # command ran before sleep
-        after_sleep_cmd = ""; # command ran after sleep
+        before_sleep_cmd = "loginctl lock-session"; # command ran before sleep
+        after_sleep_cmd = "hyprctl dispatch dpms on"; # command ran after sleep
         ignore_dbus_inhibit = false; # whether to ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
         ignore_systemd_inhibit = false; # whether to ignore systemd-inhibit --what=idle inhibitors
       };
 
       listener = {
         timeout = 300; # in seconds
-        on-timeout = "hyprlock"; # command to run when timeout has passed
+        on-timeout = "loginctl lock-session"; # command to run when timeout has passed
         on-resume = ""; # command to run when activity is detected after timeout has fired.
       };
     };
