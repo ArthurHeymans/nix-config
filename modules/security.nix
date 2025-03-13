fa022f852350bae5e7cea137f115f7442f6c8842
Arthur Heymans <arthur@aheymans.xyz>
2025-03-13 15:44:15 +0100

    Fix brave browser slow login
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- modules/security.nix
+++ modules/security.nix
@@ -2,4 +2,7 @@
   services.pcscd.enable = true;
   security.polkit.enable = true;
   security.rtkit.enable = true;
+  services.passSecretService.enable = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableKwallet = true;
 }
