0cacff4ae34fcd6b2e7ef6cc93db12ede9a5c87f
Arthur Heymans <arthur@aheymans.xyz>
2025-07-17 11:28:08 +0200

    Disable NVIDIA
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- modules/graphics.nix
+++ modules/graphics.nix
@@ -8,28 +8,28 @@
   };
 
   # Load NVIDIA driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  # services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
-    # Modesetting is required
-    modesetting.enable = true;
+  # hardware.nvidia = {
+  #   # Modesetting is required
+  #   modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (only for Turing and later architectures)
-    open = true;
+  #   # Use the NVidia open source kernel module (only for Turing and later architectures)
+  #   open = true;
 
-    # Enable the Nvidia settings menu
-    nvidiaSettings = true;
+  #   # Enable the Nvidia settings menu
+  #   nvidiaSettings = true;
 
-    # Select the appropriate driver version
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  #   # Select the appropriate driver version
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   # thunderbolt
   services.hardware.bolt.enable = true;
