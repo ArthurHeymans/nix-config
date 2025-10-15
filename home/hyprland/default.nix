52174a6ae8d359d9f18586768017a638499cd1ac
Arthur Heymans <arthur@aheymans.xyz>
2025-10-15 18:17:37 +0200

    Make emacs "other-frame" work in hyprland
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -253,6 +253,9 @@
         "suppressevent maximize, class:.*"
         # Fix some dragging issues with XWayland
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+        # Make other-frame work
+        "workspace unset, class:^(emacs)$"
+        "focusonactivate, class:^(emacs)$"
       ];
 
       # device specific settings
