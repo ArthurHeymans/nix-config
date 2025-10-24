ffb9d209883a2b3df9165f5d0970bf7960ebdb6e
Arthur Heymans <arthur@aheymans.xyz>
2025-10-24 09:56:11 +0200

    Make editing the secrets more convenient
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

---
+++ secrets/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((mode . sops)
+         (eval . (run-with-timer 0.1 nil #'sops-edit-file)))))
