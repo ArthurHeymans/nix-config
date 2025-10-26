dbb7447304426b4dec7f466042ad5a553f403bf2
Arthur Heymans <arthur@aheymans.xyz>
2025-10-26 18:29:37 +0100

    Add nix mcp server
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (add-to-list 'mcp-hub-servers '("nix" . (:command "nix" :args ("run" "github:utensils/mcp-nixos" "--"))))))))
