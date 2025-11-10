c230332c1847187a799b12890b95dbf3c866fc92
Arthur Heymans <arthur@aheymans.xyz>
2025-11-10 17:21:03 +0100

    Fix session variables
    This broke emacs
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/shell.nix
+++ home/shell.nix
@@ -19,12 +19,13 @@
     enableBashIntegration = true;
   };
 
-  home.sessionVariables = {
-    OPENAI_API_KEY = config.sops.secrets."environmentVariables/OPENAI_API_KEY".path;
-    OPENROUTER_API_KEY = config.sops.secrets."environmentVariables/OPENROUTER_API_KEY".path;
-    DEEPSEEK_API_KEY = config.sops.secrets."environmentVariables/DEEPSEEK_API_KEY".path;
-    ANTHROPIC_API_KEY = config.sops.secrets."environmentVariables/ANTHROPIC_API_KEY".path;
-  };
+  # these are not available at buildtime...
+  # home.sessionVariables = {
+  #   OPENAI_API_KEY = config.sops.secrets."environmentVariables/OPENAI_API_KEY".path;
+  #   OPENROUTER_API_KEY = config.sops.secrets."environmentVariables/OPENROUTER_API_KEY".path;
+  #   DEEPSEEK_API_KEY = config.sops.secrets."environmentVariables/DEEPSEEK_API_KEY".path;
+  #   ANTHROPIC_API_KEY = config.sops.secrets."environmentVariables/ANTHROPIC_API_KEY".path;
+  # };
 
   programs.fish = {
     enable = true;
