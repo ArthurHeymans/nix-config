9bfc6b47d9a4245475c98cae197833dcf75bfb62
Arthur Heymans <arthur@aheymans.xyz>
2025-11-19 10:10:14 +0100

    WIP add tridactyl firefox profile
    I want this to be generated from nix in the future.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/browsers.nix
+++ home/browsers.nix
@@ -8,4 +8,15 @@
   ];
 
   services.syncthing.enable = true;
+
+  xdg.desktopEntries.firefox-tridactyl = {
+    name = "Firefox (Tridactyl)";
+    genericName = "Web Browser";
+    comment = "Firefox with Tridactyl profile";
+    exec = "firefox --no-remote -P tridactyl %u";
+    icon = "firefox";
+    type = "Application";
+    categories = [ "Network" "WebBrowser" ];
+    mimeType = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
+  };
 }
