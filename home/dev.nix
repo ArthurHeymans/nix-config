ed613e35e2ff85f45096a5bb04365f4587f163eb
Arthur Heymans <arthur@aheymans.xyz>
2025-11-25 11:12:42 +0100

    probe-rs has a release with my fixes now
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/dev.nix
+++ home/dev.nix
@@ -19,26 +19,7 @@
     gopls
     cliphist
     wl-clipboard
-    (pkgs.probe-rs-tools.overrideAttrs (
-      finalAttrs: previousAttrs: {
-        version = "cda803a77e6da5fa14d312880ad8c347de14464c";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "probe-rs";
-          repo = "probe-rs";
-          rev = "cda803a77e6da5fa14d312880ad8c347de14464c";
-          hash = "sha256-eGo41KkTg9ZBces1JcmZBeOA4CwjnvWHPJL23U8Ylk8=";
-        };
-
-        # Requires IFD
-        cargoDeps = pkgs.rustPlatform.importCargoLock {
-          lockFile = finalAttrs.src + "/Cargo.lock";
-          allowBuiltinFetchGit = true;
-        };
-        cargoHash = "";
-        cargoBuildFlags = [ "--features=remote" ];
-      }
-    ))
+    probe-rs-tools
     espflash
     wlink
     gdb
