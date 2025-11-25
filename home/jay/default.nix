a46c8768660c2ab281b382a219ccf964fd5359ac
Arthur Heymans <arthur@aheymans.xyz>
2025-11-25 22:17:58 +0100

    feat(jay): Add hostname-based TrackPoint acceleration config
    Configure TrackPoint acceleration speed based on hostname, setting 1.0
    for t480-arthur and 0.0 for other systems. Replace direct file source
    with templated config using placeholder substitution.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/jay/default.nix
+++ home/jay/default.nix
@@ -1,26 +1,36 @@
-{ pkgs, ... }:
+{ pkgs, hostname, ... }:
 
 let
   jay-git = pkgs.callPackage pkgs.jay.override {
     rustPlatform = pkgs.rustPlatform // {
-      buildRustPackage = args: pkgs.rustPlatform.buildRustPackage (args // {
-        src = pkgs.fetchFromGitHub {
-          owner = "mahkoh";
-          repo = "jay";
-          rev = "a2e21cb926664cfc1980f8a38ec1aa34a7792c19";
-          hash = "sha256-uhTi2CWSuLq+mgclcZwTSp1i2BKAqs7vt2wL9MJH+UE=";
-        };
-        cargoHash = "sha256-+5+jS4dCFE8hkkHAA4BcB+xtr4UF+px9iVPuQAIijwk=";
-      });
+      buildRustPackage =
+        args:
+        pkgs.rustPlatform.buildRustPackage (
+          args
+          // {
+            src = pkgs.fetchFromGitHub {
+              owner = "mahkoh";
+              repo = "jay";
+              rev = "a2e21cb926664cfc1980f8a38ec1aa34a7792c19";
+              hash = "sha256-uhTi2CWSuLq+mgclcZwTSp1i2BKAqs7vt2wL9MJH+UE=";
+            };
+            cargoHash = "sha256-+5+jS4dCFE8hkkHAA4BcB+xtr4UF+px9iVPuQAIijwk=";
+          }
+        );
     };
   };
+
 in
 {
   home.packages = [
     jay-git
   ];
 
-  home.file.".config/jay/config.toml".source = ./config.toml;
+  home.file.".config/jay/config.toml".text =
+    let
+      accelValue = if hostname == "t480-arthur" then "1.0" else "0.0";
+    in
+    builtins.replaceStrings [ "@ACCEL_SPEED@" ] [ accelValue ] (builtins.readFile ./config.toml);
   home.file.".config/jay/screenshot.sh".source = ./screenshot.sh;
 
   # TODO add config here
