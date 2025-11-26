9347b86339da72bbbf2467a7d24d8edc129b8eef
Arthur Heymans <arthur@aheymans.xyz>
2025-11-26 11:30:41 +0100

    refactor(jay): Convert config from TOML to Nix expression
    Replace static TOML config file with a Nix function that generates the
    configuration. This enables parameterization of settings like TrackPoint
    acceleration speed based on hostname, improving maintainability and
    reducing duplication.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/jay/default.nix
+++ home/jay/default.nix
@@ -20,18 +20,18 @@ let
     };
   };
 
+  accelSpeed = if hostname == "t480-arthur" then 1.0 else 0.0;
+  jayConfig = import ./jay-config.nix { inherit accelSpeed; };
+  
+  tomlFormat = pkgs.formats.toml { };
+  jayConfigFile = tomlFormat.generate "jay-config.toml" jayConfig;
+
 in
 {
   home.packages = [
     jay-git
   ];
 
-  home.file.".config/jay/config.toml".text =
-    let
-      accelValue = if hostname == "t480-arthur" then "1.0" else "0.0";
-    in
-    builtins.replaceStrings [ "@ACCEL_SPEED@" ] [ accelValue ] (builtins.readFile ./config.toml);
+  home.file.".config/jay/config.toml".source = jayConfigFile;
   home.file.".config/jay/screenshot.sh".source = ./screenshot.sh;
-
-  # TODO add config here
 }
