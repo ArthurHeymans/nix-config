d65516130b8ad7e903fdd830aad17d7df3e7d89d
Arthur Heymans <arthur@aheymans.xyz>
2025-11-26 09:19:04 +0100

    feat(power): Replace power-profiles-daemon with TLP
    Configure TLP with performance governor on AC and powersave on battery
    for better power management control.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- modules/power.nix
+++ modules/power.nix
@@ -1,3 +1,9 @@
 {...}: {
-  services.power-profiles-daemon.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
 }
