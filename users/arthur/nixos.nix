73d5868944857804a0f43237ff446ee4859a78aa
Arthur Heymans <arthur@aheymans.xyz>
2025-12-10 15:43:06 +0100

    Add N100
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- users/arthur/nixos.nix
+++ users/arthur/nixos.nix
@@ -7,13 +7,6 @@
 
   users.users.arthur = {
     # Arthur's authorizedKeys
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKm4ucwEs89kKGpYrdhPwfnCYeVVDR3ROxqIwjK/98/7 arthur@t41sarthur"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9XttA9f8RjXTrrhli5lb4y4+uWC7K+uu2yaeLCbWCT arthur@x220-nixos"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGdvTWjJ4Q/XlCV9ziyFkWxvDsMs+veo9uQQBSPZGZB+ arthur@t14s-g6"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAB1w5zIFawrgTrTXzVfpbV9t7d/FBUm/15NZz40McEA arthur@gmktec-k11"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOIhZlW5JLnPVAWQCKGhPcDhhq0jlQamjI6wCx5UKAXZ arthur@t480-arthur"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG7kI68/elpmRp017AlpcbWPrWQwjgzcS00VsDdOJhvs arthur@x201-arthur"
-    ];
+    openssh.authorizedKeys.keys = (import ../../hosts/ssh-keys.nix).sshKeys.arthur;
   };
 }
