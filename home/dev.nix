c02dcd0744972f5d04db4339d6a567efc2cdecbc
Arthur Heymans <arthur@aheymans.xyz>
2026-03-16 07:12:29 +0100

    Add ghidra CLI
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- home/dev.nix
+++ home/dev.nix
@@ -3,6 +3,27 @@
   pkgs,
   ...
 }:
+let
+  ghidra-cli = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "ghidra-cli";
+    version = "0.1.9";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "akiselev";
+      repo = "ghidra-cli";
+      rev = "v${version}";
+      hash = "sha256-bX+lT4YeBJkOLPW+db/4CCimnLUjdc6/REk5+5PtBEE=";
+    };
+
+    cargoHash = "sha256-J+XhpIo5T/6kotHH51XEyxYLVsjJ/+p0EXTKqhef/oc=";
+
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    buildInputs = [ pkgs.openssl ];
+
+    # tests require a ghidra installation at build time
+    doCheck = false;
+  };
+in
 {
   home.packages = with pkgs; [
     rustup
@@ -26,5 +47,9 @@
     lldb
     jujutsu
     llvmPackages.bintools
+
+    # reverse engineering
+    ghidra
+    (lib.hiPrio ghidra-cli) # binary is also named `ghidra`, takes precedence over GUI
   ];
 }
