01c68116819a7bbc94d22e5669eda11477234543
Arthur Heymans <arthur@aheymans.xyz>
2026-05-28 11:05:07 +0200

    Update flake
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -7,9 +7,5 @@
   };
   services.blueman = {
     enable = true;
-    # The applet is managed by Home Manager. Leaving the NixOS applet enabled
-    # creates a duplicate blueman-applet.service drop-in and systemd rejects
-    # the unit because it gets two ExecStart entries.
-    withApplet = false;
   };
 }
