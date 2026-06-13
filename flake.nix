86b5b38678938fadeb2adccca0dfcb1179490e5f
Arthur Heymans <arthur@aheymans.xyz>
2026-06-13 02:24:25 +0200

    Try EWM Emacs buffer Screencast

--- flake.nix
+++ flake.nix
@@ -53,8 +53,8 @@
     };
 
     ewm = {
-      url = "git+https://codeberg.org/ezemtsov/ewm";
-      # url = "git+https://codeberg.org/avph/ewm?ref=input-mode-toggle";
+      # url = "git+https://codeberg.org/ezemtsov/ewm";
+      url = "git+https://codeberg.org/avph/ewm?ref=EmacsScreencast";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
