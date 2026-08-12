e3a56eb5b0323287e119fc28197c4ebf7f2c4aa7
Arthur Heymans <arthur@aheymans.xyz>
2026-08-12 10:06:09 +0200

    Remove tridactyl
    We don't use this.

--- home/browsers.nix
+++ home/browsers.nix
@@ -8,21 +8,4 @@
     # nyxt
   ];
 
-  xdg.desktopEntries.firefox-tridactyl = {
-    name = "Firefox (Tridactyl)";
-    genericName = "Web Browser";
-    comment = "Firefox with Tridactyl profile";
-    exec = "firefox --no-remote -P tridactyl %u";
-    icon = "firefox";
-    type = "Application";
-    categories = [
-      "Network"
-      "WebBrowser"
-    ];
-    mimeType = [
-      "text/html"
-      "x-scheme-handler/http"
-      "x-scheme-handler/https"
-    ];
-  };
 }
